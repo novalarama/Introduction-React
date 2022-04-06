274ca0f0cb38fb151ab9dd6e915eacad3da6e9cb
Noval AKbar Ramadhany <novalakbarramadhany@gmail.com>
2022-04-06 16:56:34 +0700

    Next Commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,11 +9,7 @@ export default function Navbar(props) {
         <button
           className="navbar-toggler"
           type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
+          
         >
           <span className="navbar-toggler-icon"></span>
         </button>
